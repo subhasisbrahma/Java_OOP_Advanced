3ef4cd216abc01ffa2c18eb546c96988e4306a00
Subhasis Brahma <subhasis.brahma222@gmail.com>
2025-11-16 18:19:56 +0530

    Added 5th encapsulation program : Constructor Chaining

---
+++ OOP/1_Encapsulation/DogConstructorChaining.java
@@ -0,0 +1,52 @@
+class Dog {
+    private String breed;
+    private float age;
+    private int price;
+
+    public Dog(String breed, float age, int price) {  // Three Arguments Constructor
+        this(breed); // Calls the one argument constructor to set defaults and the breed.
+        this.age = age;
+        this.price = price;
+    }
+
+    public Dog(String breed){ // One Argument Constructor
+        this(); // Calls the zero argument constructor to set age and price defaults.
+        this.breed = breed;
+    }
+
+    public Dog() { // Zero Argument Constructor
+        // BASE for default values
+        this.breed = "Pug";
+        this.age = 5.5f;
+        this.price = 7900;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public float getAge() { 
+        return age;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}
+
+public class DogConstructorChaining { 
+    public static void main(String[] args) {
+        
+        Dog d = new Dog("Bull Dog", 4.5f, 6500); 
+        
+        System.out.println(d.getBreed());
+        System.out.println(d.getAge());
+        System.out.println(d.getPrice());
+
+        Dog d2 = new Dog(); 
+
+        System.out.println(d2.getBreed());
+        System.out.println(d2.getAge());
+        System.out.println(d2.getPrice());
+    }
+}
