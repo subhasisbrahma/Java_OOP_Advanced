62e04eebf90c2f0bfb6cacd5e5c510d4d8377d35
Subhasis Brahma <subhasis.brahma222@gmail.com>
2025-12-09 21:41:08 +0530

    Added 5th inheritance program : Constructor Execution in inheritance CASE-1

--- OOP/3_Inheritance/ConstructorExecution1.java
+++ OOP/3_Inheritance/ConstructorExecution1.java
@@ -44,7 +44,7 @@ class Demo2 extends Demo1 {
     }
 }
 
-class ConstructorExecution1 {
+public class ConstructorExecution1 {
     public static void main(String[] args) {
 
         Demo2 d2 = new Demo2(); 
